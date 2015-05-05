1351665b7c6d351763949b17174a051683f8412b
Seckin Anlasbay <seckina@torreycommerce.com>
2015-05-05 16:33:42 -0700

    updates

--- js/rating.js
+++ js/rating.js
@@ -48,6 +48,7 @@ $(document).ready(function() {
          }
     })
     $('.star-rating-input .star-rating-star').click(function(e) {
+        alert('slicksxs');
         var elem =  $(this).parents('.star-rating-input');
         $(elem).data('chosen',true); 
         $(elem).find('.active-choosing').removeClass('choosing');  

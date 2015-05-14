7eeda1c171cb0f692a6fc422e20017b2bb3252f2
Simon Jaspar <simonj@torreycommerce.com>
2015-05-14 09:57:57 -0700

    Implemented dynamic variant displaying on product page

--- js/product.js
+++ js/product.js
@@ -1,5 +1,5 @@
 $(function() {
-    var variantManager = new VariantsManager(variants).generateSelects();
+    var variantManager = new VariantsManager(variants).init();
 });
 
 
@@ -7,9 +7,12 @@ function VariantsManager (variants) {
     var self = this;
     this.variants = variants;
 
-    this.selectInputs = {};
+    this.selectsData = {};
+    this.selectsInputs = {};
+    this.selectValues = {};
+    this.lastChangedValue = "";
 
-    defaultVariants = [ "position",
+    this.defaultVariants = [ "position",
                             "inventory_shipping_estimate",
                             "has_stock",
                             "weight",
@@ -40,69 +43,176 @@ function VariantsManager (variants) {
                             "inventory_minimum_quantity",
                             "images" ];
 
-    this.updateVariants = function(value,text){
-        console.log("CHANGE");
-        self.removeOption("size", "S");
-        self.newOption("size", "XS")
-        //$('#variation-selector-size').minimalect('change');
+    this.updateVariants = function(value, text, self){
+        var self = this;
 
+        selectName+"="+self.selectValues[selectName]
+
+        var selectName = value.split("=")[0];
+
+        if(value != this.lastChangedValue && value != selectName+"="+self.selectValues[selectName]){
+            this.lastChangedValue = value;
+            console.log("CHANGE:"+value);
+            var filteredVariants = this.getFilteredVariants();
+            console.log("filteredVariants");
+            console.log(filteredVariants);
+            var generatedSelects = this.generateSelectsData(filteredVariants);
+            console.log("generatedSelects"); 
+            console.log(generatedSelects); 
+            this.buildSelectInputs(generatedSelects);
+        }
     }
 
-    this.buildOption = function( selectName, optionValue ){
-        return $('<option>', {id: "variation-selector-"+selectName+"-"+optionValue, value: selectName+"="+optionValue, class: ""}).text(optionValue);
+    this.updateSelectInputs = function(selectData){
+        var self = this;
+        //remove from select whats not in data
+
+        //add in select whats in data
+    }
+
+    this.generateSelectsData = function(filteredVariants){
+        var self = this;
+        var selects = {};
+        $.each( this.selectsData, function(index, value){
+            selects[index] = [];
+        });
+        $.each( filteredVariants, function(index, value){
+            $.each(value, function(index, value){
+                if(self.defaultVariants.indexOf(index)<0){
+                    if(selects[index].indexOf(value)<0){
+                            selects[index].push(value);
+                    }
+                }
+            });
+          });
+
+        return selects;
+    }
+
+
+    this.getFilteredVariants = function(){
+        var filteredVariants = [];
+        var self = this;
+        //var selectValues = {};
+        //retrieve selects values 
+        $.each( self.selectsData, function(selectName, optionsArray){
+            console.log("read select value: "+selectName);
+            var selectValue = $("#variation-selector-"+selectName).val().split("=")[1];
+            self.selectValues[selectName] = selectValue;
+        });
+
+        console.log("selectValues");
+        console.log(self.selectValues);
+
+        $.each( this.variants, function(index, variant){
+            var passfilter = true;
+
+            $.each( self.selectValues, function(selectName, selectValue){
+                
+                if(selectValue != ""){
+                    if(variant[selectName]){
+                        if(variant[selectName] != selectValue){
+                            passfilter = false;
+                        }
+                    }else{
+                        passfilter = false;
+                    }
+                }
+            });
+
+            if(passfilter) filteredVariants.push(variant);
+        });
+        return filteredVariants;
+    }
+
+    this.buildOption = function( selectName, optionValue, text ){
+        return $('<option>', {id: "variation-selector-"+selectName+"-"+optionValue, value: selectName+"="+optionValue, class: ""}).text(text);
     }
 
     this.newOption = function( selectName, optionValue ){
         var value = selectName + "=" + optionValue;
-        self.selectInputs[selectName].append( self.buildOption( selectName, optionValue ) );
+        self.selectsData[selectName].append( self.buildOption( selectName, optionValue ) );
     }
 
     this.removeOption = function(selectName, optionValue){
         var value = selectName + "=" + optionValue;
-        self.selectInputs[selectName].children( "[value='"+value+"']" ).remove();
-
+        self.selectsData[selectName].children( "[value='"+value+"']" ).remove();
     }
 
-    this.generateSelects = function(){
+    this.buildSelectInputs = function(selectData){
+        var self = this;
+
+        //empty field #variation-selector before
+        
+        $.each(selectData, function(selectName, optionArray){
+            $('#variation-selector-'+selectName).minimalect("destroy");
+        })
+        $('#variation-selector').empty();
+
+        $.each(selectData, function(selectName, optionArray){
+            
+            var sel = $('<select>', {id: "variation-selector-"+selectName, name: selectName, class: "form-control"});            
+            sel.append( self.buildOption(selectName, "", "") );
+
+            $.each(optionArray, function(index, optionValue){
+                sel.append( self.buildOption(selectName, optionValue, optionValue) );
+            });
+
+            $('#variation-selector').append( $('<label>', {}).text(selectName) );
+            $('#variation-selector').append(sel);
+
+
+
+            sel.minimalect({
+                'onchange': function(value,text){
+                    self.updateVariants(value,text,self);
+                },
+                'live': true,
+                'remove_empty_option': false,
+                'debug': true,
+                'placeholder': ""
+            });
+            
+            console.log("set select value of "+selectName);
+            console.log(selectName+"="+self.selectValues[selectName]);
+            sel.val(selectName+"="+self.selectValues[selectName]);
+
+            // if(self.lastChangedValue != ""){
+            //     sel.val(self.lastChangedValue);
+            // }
+            
+        });
+    }
+
+    this.init = function(){
         var self = this;
         var selects = {};
-        //var selectInputs = {};
 
         // Build selects object containing data of the variants select tags
+
         $.each( this.variants, function(index, value){
             $.each(value, function(index, value){
-                if(defaultVariants.indexOf(index)<0){
-                    if(! selects[index] ){
-                        selects[index] = [value];
+                if(self.defaultVariants.indexOf(index)<0){
+                    if(! self.selectsData[index] ){
+                        self.selectsData[index] = [value];
                     }else{
-                        if(selects[index].indexOf(value)<0){
-                            selects[index].push(value);
+                        if(self.selectsData[index].indexOf(value)<0){
+                            self.selectsData[index].push(value);
                         }
                     }
                 }
             });
           });
 
-        //Bluilding HTML Select elements
-        $.each(selects, function(selectName, optionArray){
-            
-            var sel = $('<select>', {id: "variation-selector-"+selectName, value: selectName, name: selectName, class: "form-control"});
-            $.each(optionArray, function(index, optionValue){
-                sel.append( self.buildOption(selectName, optionValue) );
-            });
-
-            $('#variation-selector').append( $('<label>', {}).text(selectName) );
-            $('#variation-selector').append(sel);
-
-            sel.minimalect({
-                'onchange': self.updateVariants,
-                'live': true,
-                'remove_empty_option': false,
-                'debug': true
-            });
-            
-            self.selectInputs[selectName] = sel;
+        //Init selectValues
+        $.each( self.selectsData, function(index, value){
+            self.selectValues[index] = "";
         });
+        console.log("selectValues init");
+        console.log(self.selectValues);
+
+        //Bluilding HTML Select elements
+        self.buildSelectInputs(self.selectsData);
     }
 }
 
